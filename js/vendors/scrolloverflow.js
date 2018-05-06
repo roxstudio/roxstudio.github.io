5e9ec538e2d038b11ab225b7fc73da286c52a13c
Rohit joshi <30589256+Rohitjoshi9023@users.noreply.github.com>
2018-05-06 06:07:03 -0700

    23th

--- js/vendors/scrolloverflow.js
+++ js/vendors/scrolloverflow.js
@@ -1,9 +1,4 @@
-/**
-* Customized version of iScroll.js 0.0.8
-* It fixes bugs affecting its integration with fullpage.js
-* @license
-*/
-/*! iScroll v5.2.0 ~ (c) 2008-2016 Matteo Spinelli ~ http://cubiq.org/license */
+
 (function (window, document, Math) {
 var rAF = window.requestAnimationFrame  ||
     window.webkitRequestAnimationFrame  ||
